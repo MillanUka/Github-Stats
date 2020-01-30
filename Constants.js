8e55580371b70492610749c642ad96cd0d9420eb
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-01-31 11:32:51 +1300

    Adds the ability to get users events
    The application now grabs all the events

--- Constants.js
+++ Constants.js
@@ -1,3 +1,5 @@
 const API = "https://api.github.com/";
 const USER = "users/";
+const EVENT = "events";
 let currentUser;
+let events_list = [];
--- CreateGraphs.js
+++ CreateGraphs.js
@@ -2,14 +2,49 @@ document.getElementById("confirmButton").addEventListener('click', getUser);
 
 function getUser() {
     var username = document.getElementById("input").value;
-    console.log(API + USER + username);
     $.ajax({
         type: 'GET',
         url: API + USER + username,
         dataType: 'jsonp',
         success: function getProfile(data) {
-            currentUser = currentUser;
-        },
-        async: false
+            setTimeout(getProfile, 5000);
+            currentUser = data;
+            console.log(currentUser);
+            getEvents(currentUser);
+        }
     });
+}
+
+function getEvents(user) {
+    console.log(user.data.url + "/" + EVENT)
+    $.ajax({
+        type: 'GET',
+        url: user.data.url + "/" + EVENT + "?per_page=100",
+        dataType: 'jsonp',
+        success: function(data) {
+            console.log(data)
+            data.meta.Link.forEach(element => {
+                console.log(element)
+                getNextPage(element[0], data.data[0])
+            });
+            console.log()
+        }
+    });
+}
+
+function getNextPage(link, eventIDCheck) {
+    $.ajax({
+        type: 'GET',
+        url: link,
+        dataType: 'jsonp',
+        success: function(data) {
+            if (data.data[0] != eventIDCheck) {
+                events_list += data.data;
+            }
+        }
+    });
+}
+
+function getCommits(repo) {
+
 }
