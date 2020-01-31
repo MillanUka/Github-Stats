0a7f64a4aba4d78b69a5b7a045aa6ace607bd8fe
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-01-31 13:09:14 +1300

    Adds bar chart showing hours of last commits
    Adds a bar chart showing the frequency and time of the last 100 commits

--- CreateGraphs.js
+++ CreateGraphs.js
@@ -7,11 +7,11 @@ function getUser() {
         url: API + USER + username,
         dataType: 'jsonp',
         success: function getProfile(data) {
-            setTimeout(getProfile, 5000);
             currentUser = data;
-            console.log(currentUser);
+            console.log(data);
             getEvents(currentUser);
-        }
+        },
+        async: false
     });
 }
 
@@ -22,29 +22,54 @@ function getEvents(user) {
         url: user.data.url + "/" + EVENT + "?per_page=100",
         dataType: 'jsonp',
         success: function(data) {
-            console.log(data)
-            data.meta.Link.forEach(element => {
-                console.log(element)
-                getNextPage(element[0], data.data[0])
-            });
-            console.log()
-        }
+            events_list = data.data.slice();
+            calculateEventsPercentage(events_list);
+        },
+        async: false
     });
 }
 
-function getNextPage(link, eventIDCheck) {
-    $.ajax({
-        type: 'GET',
-        url: link,
-        dataType: 'jsonp',
-        success: function(data) {
-            if (data.data[0] != eventIDCheck) {
-                events_list += data.data;
-            }
-        }
+function calculateEventsPercentage(events) {
+    console.log(events_list);
+
+    events_list.forEach(element => {
+        console.log(element);
+        var date = new Date(element.created_at);
+        console.log(date)
+        console.log(date.getHours())
+        timeData[date.getHours()][1]++;
     });
+
+    drawCharts();
 }
 
-function getCommits(repo) {
+function drawCharts() {
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', { 'packages': ['corechart'] });
 
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'Hour');
+        data.addColumn('number', 'Frequency');
+        data.addRows(timeData);
+
+        // Set chart options
+        var options = {
+            'title': 'Frequency of Events',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.BarChart(document.getElementById('chartDiv'));
+        chart.draw(data, options);
+    }
 }
